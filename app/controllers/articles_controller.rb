d551db7641d98284b63924158669d341a7239ec0
Suraj Mandal <suraj.mandal738@gmail.com>
2019-12-15 21:05:21 +0530

    extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   # method used to list the articles present in the db and show to the webpage
   def index
     @articles = Article.all
@@ -14,7 +16,7 @@ class ArticlesController < ApplicationController
   # here all the validations are made
   def create
     #render plain: params[:article].inspect
-    @article = Article.new(article_params)
+
     if @article.save
       # do something
       flash[:notice] = "Article was successfully created"
@@ -27,11 +29,9 @@ class ArticlesController < ApplicationController
 
   # renders the form for editing the article
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -42,17 +42,20 @@ class ArticlesController < ApplicationController
 
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = 'Article was successfully deleted'
     redirect_to articles_path
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     # from the params hash we permit to use these values and enter those into the article.new params
     params.require(:article).permit(:title, :description)
