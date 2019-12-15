d6f56f3e94222709c487aea503b0f85e295e4e88
Suraj Mandal <suraj.mandal738@gmail.com>
2019-12-15 20:37:21 +0530

    Added the update and create options to the ui and built the functionality at the backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
 
+  # method used to list the articles present in the db and show to the webpage
+  def index
+    @articles = Article.all
+  end
+
+  # form for creating a new article
   def new
     @article = Article.new
   end
 
+  # function being invoked when the article is being created after pressing the create article submit btn.
+  # here all the validations are made
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -16,6 +24,22 @@ class ArticlesController < ApplicationController
     end
     #redirect_to article_path(@article)
   end
+
+  # renders the form for editing the article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
 
   def show
     @article = Article.find(params[:id])
